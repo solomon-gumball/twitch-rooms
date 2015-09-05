90f709a20cbf335ced20ccdcd29b6b787577b0d5
Joseph Sample <redwood@Josephs-MacBook-Pro.local>
2015-09-05 16:13:10 -0700

    fixing isOpera bug

--- src/browserCompatability.js
+++ src/browserCompatability.js
@@ -1,6 +1,6 @@
 export default function testBrowseryCompatability() {
 
-	var isChrome = !!window.chrome && !isOpera;
+	var isChrome = !!window.chrome;
 	var isFirefox = typeof InstallTrigger !== 'undefined';
 
 	if (!isChrome || !isFirefox) {
